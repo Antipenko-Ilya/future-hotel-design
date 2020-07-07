e372608821e92ce9fa2aeb5f7e9e2fcbbe27ce3a
Ilya <mix-xim.zk@ande.ru>
2020-07-08 00:10:30 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      future-hotel-design
+    </div>
+  );
+}
+
+export default App;
